1e79085a396dff27f74838d664c901a987356604
Fabrizio Bertoglio <fabrizio.bertoglio@gmail.com>
2023-12-12 20:48:49 +0800

    over-ride measure function and apply patch #41770 (#3)
    * feat: measure func is over-ridden by parent ReactTextShadowNode
    
    * change width of the ReactTextShadowNode
    
    * original impl of ReactTextShadowNode
    
    * apply patch #41770
    
    https://github.com/facebook/react-native/pull/41770

--- android/src/main/java/com/text/ReactTextViewImprovedManager.java
+++ android/src/main/java/com/text/ReactTextViewImprovedManager.java
@@ -1,31 +1,46 @@
 package com.text;
 
-import android.graphics.Color;
-import android.widget.TextView;
-
-import androidx.annotation.Nullable;
 import com.facebook.react.module.annotations.ReactModule;
-import com.facebook.react.uimanager.IViewManagerWithChildren;
 import com.facebook.react.uimanager.ThemedReactContext;
-import com.facebook.react.uimanager.annotations.ReactProp;
-import com.facebook.react.views.text.ReactTextAnchorViewManager;
 import com.facebook.react.views.text.ReactTextShadowNode;
 import com.facebook.react.views.text.ReactTextView;
 import com.facebook.react.views.text.ReactTextViewManager;
+import com.facebook.react.views.text.ReactTextViewManagerCallback;
+
+import androidx.annotation.Nullable;
 
 @ReactModule(name = ReactTextViewImprovedManager.NAME)
 public class ReactTextViewImprovedManager
   extends ReactTextViewManager {
-  // Need to rename to ReactTextViewImproved
   public static final String NAME = "RCTTextImproved";
+  protected @Nullable ReactTextViewManagerCallback mReactTextViewManagerCallback;
 
   @Override
   public String getName() {
     return NAME;
   }
 
+  public ReactTextViewImprovedManager() {
+    this(null);
+  }
+
+  public ReactTextViewImprovedManager(@Nullable ReactTextViewManagerCallback reactTextViewManagerCallback) {
+    super(reactTextViewManagerCallback);
+    mReactTextViewManagerCallback = reactTextViewManagerCallback;
+  }
+
   @Override
   public ReactTextView createViewInstance(ThemedReactContext context) {
     return new ReactTextViewImproved(context);
+  }
+
+  @Override
+  public ReactTextShadowNode createShadowNodeInstance() {
+    return new ReactTextViewImprovedShadowNode(mReactTextViewManagerCallback);
+  }
+
+  @Override
+  public Class<ReactTextShadowNode> getShadowNodeClass() {
+    return ReactTextShadowNode.class;
   }
 }
--- example/src/App.tsx
+++ example/src/App.tsx
@@ -4,33 +4,36 @@ import { View, StyleSheet, Text, TextInput } from 'react-native';
 import { TextViewImproved } from 'react-native-text';
 
 export default function App() {
+  const email =
+    'From vincenzoddragon+five@gmail.com  From vincenzoddrlxagon+five@gmail.com';
   return (
-    <View>
-      <TextViewImproved style={styles.parentText}>
-        This is the new text
-      </TextViewImproved>
-      <Text>This is normal text</Text>
-      <TextInput style={styles.input} />
-    </View>
+    <>
+      <View style={styles.container}>
+        <View style={styles.flexBrokenStyle}>
+          <TextViewImproved
+            textBreakStrategy="simple"
+            style={styles.parentText}
+            numberOfLines={1}
+          >
+            {email}
+          </TextViewImproved>
+        </View>
+      </View>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
     justifyContent: 'center',
+    padding: 8,
+    backgroundColor: 'yellow',
+  },
+  flexBrokenStyle: {
+    flexDirection: 'row',
   },
   parentText: {
-    width: 160,
-    height: 60,
-    marginVertical: 20,
     backgroundColor: 'red',
-  },
-  childText: {
-    color: 'blue',
-  },
-  input: {
-    borderWidth: 1,
   },
 });
