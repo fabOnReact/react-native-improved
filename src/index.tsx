feb180dd4395ec48a0a0fae6bd51ef26f5e956d0
Fabrizio Bertoglio <fabrizio.bertoglio@gmail.com>
2023-12-13 17:13:40 +0800

    add copyright headers

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,10 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
 import createReactNativeComponentClass from 'react-native/Libraries/Renderer/shims/createReactNativeComponentClass';
 import { createViewConfig } from 'react-native/Libraries/NativeComponent/ViewConfig';
 import { Text, Platform } from 'react-native';
