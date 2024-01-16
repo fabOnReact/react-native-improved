f9dc57db60fd61d66eda8284094cbe70c511be3e
Fabrizio Bertoglio <fabrizio.bertoglio@gmail.com>
2024-01-16 16:27:35 +0800

    renderer patch

---
+++ example/react_native_renderer_patch.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+rm ../react-native/Libraries/Renderer/implementations/ReactNativeRenderer-dev.js
+cp ./ReactNativeRenderer-dev.js ../react-native/Libraries/Renderer/implementations/ReactNativeRenderer-dev.js
