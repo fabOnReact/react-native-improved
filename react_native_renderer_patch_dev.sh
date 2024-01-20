6e4c37b879dcd4b686b2771ae265ce0809b50eb5
Fabrizio Bertoglio <fabrizio.bertoglio@gmail.com>
2024-01-20 12:02:35 +0800

    patch works ios and js

--- react_native_renderer_patch_dev.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-rm ./example/node_modules/react-native/Libraries/Renderer/implementations/ReactNativeRenderer-dev.js
-cp ./ReactNativeRenderer-dev.js ./example/node_modules/react-native/Libraries/Renderer/implementations/ReactNativeRenderer-dev.js
