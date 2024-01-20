3f23a3007334e31906f26e7aa79aa077151bba9e
Fabrizio Bertoglio <fabrizio.bertoglio@gmail.com>
2024-01-20 16:25:59 +0800

    Clean up files and add a Text example (#12)
    * clean up files
    
    * adding text example

--- example/react_native_renderer_patch.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-rm ../react-native/Libraries/Renderer/implementations/ReactNativeRenderer-dev.js
-cp ./ReactNativeRenderer-dev.js ../react-native/Libraries/Renderer/implementations/ReactNativeRenderer-dev.js
