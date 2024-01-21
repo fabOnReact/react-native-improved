3375308b1c76a8a8bc9e114edca10edf60b50308
Fabrizio Bertoglio <fabrizio.bertoglio@gmail.com>
2024-01-21 12:01:20 +0800

    adding ReactEditTextImproved native API

---
+++ android/src/main/java/com/textinput/ReactEditTextImproved.java
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+package com.textinput;
+
+import com.facebook.react.bridge.ReactContext;
+import com.facebook.react.views.textinput.ReactEditText;
+
+public class ReactEditTextImproved extends ReactEditText {
+
+  public ReactEditTextImproved(ReactContext context) {
+    super(context);
+  }
+}
