43f03cd30240531a5ecca995c23e69a04836d1cb
Fabrizio Bertoglio <fabrizio.bertoglio@gmail.com>
2024-01-26 14:03:06 +0800

    basic implementation of modal on android to avoid runtime

---
+++ android/src/main/java/com/modal/ReactModalHostManagerImproved.java
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+package com.modal;
+
+import com.facebook.react.module.annotations.ReactModule;
+import com.facebook.react.views.modal.ReactModalHostManager;
+
+/** View manager for {@link ReactModalHostView} components. */
+@ReactModule(name = ReactModalHostManager.REACT_CLASS)
+public class ReactModalHostManagerImproved extends ReactModalHostManager {
+
+  public static final String REACT_CLASS = "RCTModalHostViewImproved";
+
+  @Override
+  public String getName() {
+    return REACT_CLASS;
+  }
+}
