643f3ba7e687f9d8191ef6b32a9ae83a160ef6de
Fabrizio Bertoglio <fabrizio.bertoglio@gmail.com>
2024-01-30 16:08:11 +0800

    android building with android studio

--- example/android/app/src/main/java/com/improvedexample/MainActivity.kt
+++ example/android/app/src/main/java/com/improvedexample/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.textexample
+package com.improvedexample
 
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
--- example/android/app/src/main/java/com/improvedexample/MainApplication.kt
+++ example/android/app/src/main/java/com/improvedexample/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.textexample
+package com.improvedexample
 
 import android.app.Application
 import com.facebook.react.PackageList
@@ -11,6 +11,7 @@ import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
 import com.facebook.react.defaults.DefaultReactNativeHost
 import com.facebook.react.flipper.ReactNativeFlipper
 import com.facebook.soloader.SoLoader
+import com.improvedexample.BuildConfig
 
 class MainApplication : Application(), ReactApplication {
 
